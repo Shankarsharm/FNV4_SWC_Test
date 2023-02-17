6cb09312523197b9dbc6b77b59e3d558c05823e7
Shankarsharm <80143973+Shankarsharm@users.noreply.github.com>
2023-02-17 14:45:47 +0530

    Update sync.sh

--- .github/runfiles/sync.sh
+++ .github/runfiles/sync.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
-len=$(yq "length" test.yaml)
+len=$(yq "length" manifest.yml)
 
 for ((i=0; i<${len}; i++))
 do
   #Collecting Values from Yaml file
-  repo=$(yq ".[$i].Repository" test.yaml)
-  name=$(yq ".[$i].Name" test.yaml)
-  branch=$(yq ".[$i].Branch" test.yaml)
-  Sync_dir=$(yq ".[$i].Sync_dir" test.yaml)
+  repo=$(yq ".[$i].Repository" manifest.yml)
+  name=$(yq ".[$i].Name" manifest.yml)
+  branch=$(yq ".[$i].Branch" manifest.yml)
+  Sync_dir=$(yq ".[$i].Sync_dir" manifest.yml)
   echo "$repo"
-  echo "git clone -b $branch https://Shankarsharm:$1@$repo"
+  git clone -b "$branch" https://Shankarsharm:"$1"@"$repo"
   if [[ $Sync_dir == "all" ]]
   then
     echo "yes copy everything"
     echo "$name"
-    #cd $2
-    #rm -rf .git
-    #cp -R . $name/$2
-    #git add .
-    #git commit -m "Has added files"
-    #git push https://Shankarsharm:$1@$repo
+    cd "$2"
+    ls 
+    pwd
+    rm -rf .git
+    cp -R . "$name"/"$2"
+    git add .
+    git commit -m "Has added files"
+    git push https://Shankarsharm:"$1"@"$repo"
 
   else
     echo "Need to copy specific Directories"
@@ -28,12 +30,14 @@ do
     for ((j=1; j<=${dir}+1; j++))
     do
       file=$(echo $Sync_dir | cut -d "," -f"$j")
-      echo $file
-      #cd $2
-      #cp $file $name/$2
+      echo "$file"
+      cd "$2"
+      ls 
+      pwd
+      cp "$file" "$name"/"$2"
     done
-      #git add .
-      #git commit -m "Added files"
-      #git push https://Shankarsharm:$1@$repo
+      git add .
+      git commit -m "Added files"
+      git push https://Shankarsharm:"$1"@"$repo"
   fi
 done
